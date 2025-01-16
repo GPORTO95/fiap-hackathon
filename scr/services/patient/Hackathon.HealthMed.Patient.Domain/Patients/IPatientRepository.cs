ec2a48a30e8347648888cf62d30a111045942030
Gabriel Porto <gr_porto@hotmail.com>
2025-01-15 23:08:14 -0300

    feat: add interface email unique

--- scr/services/patient/Hackathon.HealthMed.Patient.Domain/Patients/IPatientRepository.cs
+++ scr/services/patient/Hackathon.HealthMed.Patient.Domain/Patients/IPatientRepository.cs
@@ -5,6 +5,8 @@ namespace Hackathon.HealthMed.Patient.Domain.Patients;
 public interface IPatientRepository
 {
     Task<bool> IsCpfUniqueAsync(Cpf cpf, CancellationToken cancellationToken = default);
-
+    
+    Task<bool> IsEmailUniqueAsync(Email email, CancellationToken cancellationToken = default);
+    
     void Insert(Patient patient);
 }
